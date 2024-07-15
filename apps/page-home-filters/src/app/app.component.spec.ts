f0da213e4cbbdf89a5cf7a4396730bcadcc00adf
C1325334 Elvis Thermo Carvalho Miranda <c1325334@interno.bb.com.br>
2024-07-15 14:43:29 -0300

    change files to build

--- apps/page-home-filters/src/app/app.component.spec.ts
+++ apps/page-home-filters/src/app/app.component.spec.ts
@@ -1,33 +1,26 @@
-// import { TestBed } from '@angular/core/testing';
-// import { AppComponent } from './app.component';
-// import { RouterTestingModule } from '@angular/router/testing';
-// import { HomeComponent } from './home/home.component';
-// import { ContentTableComponent } from './content-table/content-table.component';
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
-// describe('AppComponent', () => {
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [
-//         AppComponent,
-//         HomeComponent,
-//         ContentTableComponent,
-//         RouterTestingModule,
-//       ],
-//     }).compileComponents();
-//   });
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent, RouterTestingModule],
+    }).compileComponents();
+  });
 
-//   // it('should render title', () => {
-//   //   const fixture = TestBed.createComponent(AppComponent);
-//   //   fixture.detectChanges();
-//   //   const compiled = fixture.nativeElement as HTMLElement;
-//   //   expect(compiled.querySelector('h1')?.textContent).toContain(
-//   //     'Welcome page-home-filters'
-//   //   );
-//   // });
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Welcome page-home-filters'
+    );
+  });
 
-//   // it(`should have as title 'page-home-filters'`, () => {
-//   //   const fixture = TestBed.createComponent(AppComponent);
-//   //   const app = fixture.componentInstance;
-//   //   expect(app.title).toEqual('page-home-filters');
-//   // });
-// });
+  it(`should have as title 'page-home-filters'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('page-home-filters');
+  });
+});
